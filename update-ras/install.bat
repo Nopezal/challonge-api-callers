333338d6829a14e28412c0e797cbcdb5b581ac3a
joniauw <78723248+joniauw@users.noreply.github.com>
2021-09-26 15:34:12 +0200

    oops x2

--- update-ras/install.bat
+++ update-ras/install.bat
@@ -1,1 +1,3 @@
+cd /d "%localappdata%/Programs/Python/Python39/Scripts/"
 cmd /c "pip install pychallonge"
+pause
--- update-ras/update-ras.bat
+++ update-ras/update-ras.bat
@@ -1,1 +1,2 @@
 cmd /c "py update-ras.py username-here api-key-here domain-here subdomain-here-optional"
+pause
